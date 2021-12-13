74fc03ae90c00ac542e8a84a3f03f49ce26f55c1
Matias <matiasgsartori@gmail.com>
2021-12-13 10:19:08 -0300

    Menu principal con seleccion de Theme

--- app/src/main/java/com/example/calcularpies/MainActivity.java
+++ app/src/main/java/com/example/calcularpies/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.calcularpies;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,7 +31,7 @@ import java.text.DecimalFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity
 {
     private EditText txtEspesor;
     private EditText txtAncho;
@@ -35,6 +42,12 @@ public class MainActivity extends Activity
     private TextView totalPies;
 
     private static final double CONSTANTE = 0.2734;
+
+    SharedPreferences sharedPreferences;
+
+    String tema;
+
+    int temaSeleccionado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -52,8 +65,6 @@ public class MainActivity extends Activity
 
         totalPies = findViewById(R.id.labelTotalPies);
 
-        Button btnLimpiar = findViewById(R.id.btnLimpiar);
-
         // formateamos su entrada de datos
         txtEspesor.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(3, 2)});
         txtAncho.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(3, 2)});
@@ -64,6 +75,13 @@ public class MainActivity extends Activity
         pickerCantidad.setUnit(1);
         pickerCantidad.setValue(1);
 
+        sharedPreferences = getSharedPreferences("Theme", Context.MODE_PRIVATE);
+
+        System.out.println("sharedPreferences.getAll() --> " + sharedPreferences.getAll());
+
+        tema = sharedPreferences.getString("ThemeName", "Default");
+
+        aplicarTema(tema);
 
         // le asignamos un textChangedListener
         darListeners();
@@ -87,6 +105,27 @@ public class MainActivity extends Activity
         super.onRestoreInstanceState(savedInstanceState);
 
         totalPies.setText(savedInstanceState.getString("totalpies"));
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.tema:
+                mostrarTemasDialog();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     class DecimalDigitsInputFilter implements InputFilter
@@ -214,5 +253,62 @@ public class MainActivity extends Activity
         this.pickerCantidad.setValue(1);
 
         this.txtEspesor.requestFocus();
+    }
+
+    private void mostrarTemasDialog()
+    {
+        String [] temas = {getString(R.string.claro), getString(R.string.oscuro)};
+
+        int temaActual = !tema.equalsIgnoreCase("Oscuro") && !tema.equalsIgnoreCase("Dark") ? 0 : 1;
+
+        temaSeleccionado = temaActual;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(getString(R.string.seleccionar_tema));
+        builder.setSingleChoiceItems(temas, temaActual, (dialog, which) ->
+        {
+            temaSeleccionado = which;
+        });
+
+        builder.setPositiveButton(getString(R.string.aceptar), (dialog, which) ->
+        {
+            if(temaSeleccionado != temaActual)
+            {
+                tema = temas[temaSeleccionado];
+
+                guardarTema(tema);
+            }
+
+            dialog.dismiss();
+        });
+        builder.setNegativeButton(getString(R.string.cancelar), (dialog, which) ->
+        {
+
+            dialog.dismiss();
+        });
+
+        builder.show();
+    }
+
+    private void aplicarTema(String nombreTema)
+    {
+        if(!nombreTema.equalsIgnoreCase("Oscuro") && !nombreTema.equalsIgnoreCase("Dark"))
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        System.out.println("SE APLICO EL TEMA! --> " + nombreTema);
+    }
+
+    private void guardarTema(String nombreTema)
+    {
+        // Create preference to store theme name
+        SharedPreferences preferences = getSharedPreferences("Theme", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("ThemeName", nombreTema);
+        editor.apply();
+        recreate();
+
+        System.out.println("SE GUARDO EL TEMA --> " + nombreTema);
     }
 }
